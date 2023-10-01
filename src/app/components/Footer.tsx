d6c7a8b706833752b762d534aa481e5da35fa05a
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-10-01 15:34:25 +0100

    Add footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-orange-400 flex items-center justify-between'>
+            <Link href="/" className='font-bold text-xl'>Fasty Pasty</Link>
+            <p> ALL RIGHTS RESERVED.</p>
+
         </div>
     )
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,18 +9,21 @@ import CartIcon from './CartIcon'
 const Navbar = () => {
     const user = false
     return (
-        <div className='h-12 text-blue-500 p-4 flex items-center justify-between border-b-2 border-b-blue-500 uppercase'>
+        <div className='h-12 text-blue-500 p-4 flex items-center justify-between border-b-2 border-b-blue-500 uppercase md:h-20 lg:px-20 xl:px-40'>
             <div className='hidden md:flex gap-4 flex-1'>
                 <Link href="/">Homepage</Link>
                 <Link href="menu">Menu</Link>
-                <Link href="/">Contact</Link>
+                <Link href="menu">Contact</Link>
+
             </div>
-            <div className='text-xl md:font-bold flex-1'><Link href="/">Fasty Pasty</Link></div>
+            <div className='text-xl md:font-bold flex-1 md:text-center'>
+                <Link href="/">Fasty Pasty</Link>
+            </div>
             <div className='md:hidden'>
                 <Menu />
             </div>
-            <div className='hidden md:flex gap-4 items-center flex-1'>
-                <div className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+            <div className='hidden md:flex gap-4 items-center justify-end flex-1 justi'>
+                <div className='md:absolute top-3 r-3 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
                     <Image src="/phone.png" alt="" width={20} height={20} />
                     <span>23 200300</span>
                 </div>
